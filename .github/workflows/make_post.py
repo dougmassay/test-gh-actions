c572ec395026c809a188163b463f60f447287911
Doug Massay <douglaslmassay@gmail.com>
2021-06-30 21:03:21 -0400

    Use event json

--- .github/workflows/make_post.py
+++ .github/workflows/make_post.py
@@ -41,32 +41,19 @@ def main(argv):
         elif opt in ("-p", "--path"):
             path = arg
 
-    fi = os.path.join(path, 'details.txt')
-    body = os.path.join(path, 'contents.txt')
-    with codecs.open(fi, 'r', 'utf-8') as f:
-        details = json.load(f)
-    with codecs.open(body, 'r', 'utf-8') as f:
-        contents = f.read()
+    with codecs.open(path, 'r', 'utf-8') as f:
+        j = json.load(f)
+    r = j['release']
+    date = parser.parse(r['published_at'])
+    title = r['name']
 
-    date = parser.parse(details['published_date'])
-    title = details['release_name']
-
-    filename = os.path.join(path, date.strftime('%Y-%m-%d') + '-' + title.lower().replace(' ', '-') + '.md')
+    filename = os.path.join('.', date.strftime('%Y-%m-%d') + '-' + title.lower().replace(' ', '-') + '.md')
     print(filename)
-    md = POST.format(title, details['published_date'], 'Sigil', contents)
+    md = POST.format(title, r['published_at'], 'Sigil', r['body'])
     print(md)
     with codecs.open(filename, 'w', 'utf-8') as f:
         f.write(md)
 
-    '''
-    try:
-        with open(p, 'w') as f:
-            f.write('\n'.join(remote))
-            f.write('\n')
-    except Exception:
-        sys.exit(2)
-    print('Updates from remote versions')
-    '''
     sys.exit()
 
 if __name__ == "__main__":
